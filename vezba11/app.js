e5573ecfe6e33f79e0459a7c3cc02c48e39fad46
Imad Midovic <100241368+NpImad@users.noreply.github.com>
2023-05-31 11:06:01 +0200

    Add files via upload

---
+++ vezba11/app.js
@@ -0,0 +1,41 @@
+// class Shop {
+//     ime;
+//     lokacija;
+//     logo;
+
+//     constructor(ime, lokacija, MaXi){
+//         this.name = ime;
+//         this.lokation = lokacija;
+//         this.logo = MaXi;
+//     }
+//     get ourName(){
+//         return this.#ime;
+//     }
+// }
+
+// const maxi = new Shop("maxi", "1. may", "MaXi")
+// console.log(maxi);
+
+// const imeProdavnice = maxi.ourName;
+// console.log(imeProdavnice);
+
+
+// class Mi {
+//     ime;
+//     prezime;
+//     #godina;
+
+//     constructor(name, surname, year){
+//     this.ime = name;
+//     this.prezime = surname;
+//     this.#godina = year;
+//     }
+//     getourYear(){
+//         return this.#godina;
+//     }
+
+// }
+
+
+// const Ja = new Mi ("Imad", "Midovic", "17")
+// console.log(Ja);

99247443b7e04469beab7a7a70a456e9b496bd6e
Imad Midovic <100241368+NpImad@users.noreply.github.com>
2023-06-14 11:27:35 +0200

    Add files via upload

---
+++ vezba13/app.js
@@ -0,0 +1,44 @@
+// sayHello();
+
+// function sayHello(){
+//     console.log("John doe");
+// }
+
+// nekaFunkcija();
+
+// const nekaFunkcija = () => {
+//     console.log("John doe");
+// }
+
+
+
+// var hisName = "Muzafer";
+// greetings(hisName);
+
+// function greetings(nekoIme){
+//     console.log(nekoIme);
+// }
+
+// var hisName = "Muzafer1234%^&*";
+// let brojac = 0;
+
+// for(let i = 0; i < hisName.length; i++){
+//     if(+hisName[i] / 1 === +hisName[i]){
+//         console.log('Ima');
+//     }else{
+//         brojac++;
+//     }
+// }
+// console.log(brojac);
+
+// const automobili = {
+//     ja: ["vw", "audi"],
+//     tajra: ["golf", "audi", "bmw"],
+//     iman:["peugeot", "fiat", "citroen","X5"],
+//     bronja:["pagani", "McLaren","rollce roys"],
+// }
+// const allCars = Object.values(automobili);
+
+// const drugiAutomobili = allCars.map((car) => [...car,car.lenght]);
+
+// console.log(drugiAutomobili);
